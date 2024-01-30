a80802e738cf8c74dffcd126e5469283249760cd
sirvin01 <salimjr201@gmail.com>
2024-01-30 14:38:39 +0300

    Final

--- Recognize.py
+++ Recognize.py
@@ -5,17 +5,20 @@ import time
 import cv2
 import pandas as pd
 
-
 #-------------------------
-def recognize_attendence():
-    recognizer = cv2.face.LBPHFaceRecognizer_create()  # cv2.createLBPHFaceRecognizer()
+def recognize_attendance():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
     recognizer.read("./TrainingImageLabel/Trainner.yml")
     harcascadePath = "haarcascade_frontalface_default.xml"
     faceCascade = cv2.CascadeClassifier(harcascadePath)
     df = pd.read_csv("StudentDetails"+os.sep+"StudentDetails.csv")
     font = cv2.FONT_HERSHEY_SIMPLEX
     col_names = ['Id', 'Name', 'Date', 'Time']
-    attendance = pd.DataFrame(columns=col_names)
+    attendance_file = f"Attendance_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+
+    # Create the attendance file with the header
+    with open(attendance_file, 'w') as file:
+        file.write(','.join(col_names) + '\n')
 
     # Initialize and start realtime video capture
     cam = cv2.VideoCapture(0)
@@ -26,61 +29,61 @@ def recognize_attendence():
     minH = 0.1 * cam.get(4)
 
     while True:
-        _,im = cam.read()
+        _, im = cam.read()
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, 1.2, 5,minSize = (int(minW), int(minH)),flags = cv2.CASCADE_SCALE_IMAGE)
-        for(x, y, w, h) in faces:
+        faces = faceCascade.detectMultiScale(gray, 1.2, 5, minSize=(int(minW), int(minH)), flags=cv2.CASCADE_SCALE_IMAGE)
+        
+        for (x, y, w, h) in faces:
             cv2.rectangle(im, (x, y), (x+w, y+h), (10, 159, 255), 2)
             Id, conf = recognizer.predict(gray[y:y+h, x:x+w])
 
             if conf < 100:
-
                 aa = df.loc[df['Id'] == Id]['Name'].values
                 confstr = "  {0}%".format(round(100 - conf))
                 tt = str(Id)+"-"+aa
-
-
             else:
                 Id = '  Unknown  '
                 tt = str(Id)
                 confstr = "  {0}%".format(round(100 - conf))
 
-            if (100-conf) > 67:
+            if (100-conf) > 51:
                 ts = time.time()
                 date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
                 timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
                 aa = str(aa)[2:-2]
-                attendance.loc[len(attendance)] = [Id, aa, date, timeStamp]
+                
+                # Check if the ID already exists in the file
+                attendance_df = pd.read_csv(attendance_file)
+                if int(Id) not in attendance_df['Id'].values:
+                    with open(attendance_file, 'a') as file:
+                        file.write(f"{Id},{aa},{date},{timeStamp}\n")
+                    print(f"Details captured for {aa} at {date} {timeStamp}")
+
+                # Check if there are no other faces in the frame
+                if len(faces) == 1:
+                    cam.release()
+                    cv2.destroyAllWindows()
+                    return
 
             tt = str(tt)[2:-2]
-            if(100-conf) > 67:
+            if (100-conf) > 51:
                 tt = tt + " [Pass]"
-                cv2.putText(im, str(tt), (x+5,y-5), font, 1, (255, 255, 255), 2)
+                cv2.putText(im, str(tt), (x+5, y-5), font, 1, (255, 255, 255), 2)
             else:
                 cv2.putText(im, str(tt), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
 
-            if (100-conf) > 67:
-                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font,1, (0, 255, 0),1 )
+            if (100-conf) > 51:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 255, 0), 1)
             elif (100-conf) > 50:
                 cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 255, 255), 1)
             else:
                 cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 0, 255), 1)
 
-
-
-        attendance = attendance.drop_duplicates(subset=['Id'], keep='first')
         cv2.imshow('Attendance', im)
         if (cv2.waitKey(1) == ord('q')):
             break
-    ts = time.time()
-    date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
-    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-    Hour, Minute, Second = timeStamp.split(":")
-    fileName = "Attendance"+os.sep+"Attendance_"+date+"_"+Hour+"-"+Minute+"-"+Second+".csv"
-    attendance.to_csv(fileName, index=False)
+
     print("Attendance Successful")
-    cam.release()
-    cv2.destroyAllWindows()
 
 # Call the function
 # recognize_attendance()
